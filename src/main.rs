38b1852f92ad9c067c32412e26f6934f4656d0fe
J.c <Jak-Ch-ll@mailbox.org>
2026-03-22 17:43:25 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
-use std::io::stdin;
-use std::io::{self, Write};
+use std::{
+    fmt::Display,
+    io::{self, Write},
+};
+
+enum Command {
+    Exit,
+    Echo(String),
+    Type(String),
+    Unknown(String),
+}
+
+impl From<String> for Command {
+    fn from(value: String) -> Self {
+        let (command, arguments) = value.trim().split_once(' ').unwrap_or((value.trim(), ""));
+
+        match command {
+            "exit" => Self::Exit,
+            "echo" => Self::Echo(arguments.trim().into()),
+            "type" => Self::Type(arguments.trim().into()),
+            unknown => Self::Unknown(unknown.trim().into()),
+        }
+    }
+}
+
+impl Display for Command {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Command::Exit => "exit",
+                Command::Echo(_) => "echo",
+                Command::Type(_) => "type",
+                Command::Unknown(_) => "unknown",
+            }
+        )
+    }
+}
 
 fn main() {
     loop {
@@ -7,16 +44,16 @@ fn main() {
         io::stdout().flush().unwrap();
 
         let mut command = String::new();
-        stdin().read_line(&mut command).unwrap();
+        io::stdin().read_line(&mut command).unwrap();
 
-        match command.trim() {
-            "exit" => break,
-            command if command.starts_with("echo") => println!("{}", &command[5..]),
-            command if command.starts_with("type") => match &command[5..] {
-                c @ ("echo" | "type" | "exit") => println!("{} is a shell builtin", c),
-                c => println!("{}: not found", c),
+        match Command::from(command) {
+            Command::Exit => break,
+            Command::Echo(arguments) => println!("{}", arguments),
+            Command::Type(command) => match command.into() {
+                Command::Unknown(command) => println!("{}: not found", command),
+                c => println!("{} is a shell builtin", c),
             },
-            command => println!("{}: command not found", command),
+            Command::Unknown(command) => println!("{}: command not found", command),
         }
     }
 }

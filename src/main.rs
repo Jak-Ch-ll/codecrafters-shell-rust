25bf8a034a131e23d37c1e7af6ebb30c3c3e0caf
J.c <Jak-Ch-ll@mailbox.org>
2026-03-22 15:32:38 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-#[allow(unused_imports)]
+use std::io::stdin;
 use std::io::{self, Write};
 
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+    stdin().read_line(&mut command).unwrap();
+
+    println!("{}: command not found\n", command.trim());
 }

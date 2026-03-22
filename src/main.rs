35066dd57efaeec3d01e6dd071cea958ba9d8add
J.c <Jak-Ch-ll@mailbox.org>
2026-03-22 16:33:18 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,10 @@ fn main() {
         match command.trim() {
             "exit" => break,
             command if command.starts_with("echo") => println!("{}", &command[5..]),
+            command if command.starts_with("type") => match &command[5..] {
+                c @ ("echo" | "type" | "exit") => println!("{} is a shell builtin", c),
+                c => println!("{}: not found", c),
+            },
             command => println!("{}: command not found", command),
         }
     }

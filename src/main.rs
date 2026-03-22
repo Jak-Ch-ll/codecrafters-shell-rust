57b4adc5138593d1cb69d6f8505e82b76fd06f85
J.c <Jak-Ch-ll@mailbox.org>
2026-03-22 15:44:29 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use std::io::stdin;
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let mut command = String::new();
-    stdin().read_line(&mut command).unwrap();
+        let mut command = String::new();
+        stdin().read_line(&mut command).unwrap();
 
-    println!("{}: command not found\n", command.trim());
+        println!("{}: command not found\n", command.trim());
+    }
 }

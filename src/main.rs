6ed3591a4cc25ac47682788241e6eed0f4cc9ac6
J.c <Jak-Ch-ll@mailbox.org>
2026-03-23 11:42:27 +0100

    feat: run a program

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::{
     ffi::OsString,
     fs,
     io::{self, Write},
+    process,
 };
 
 fn main() {
@@ -19,7 +20,12 @@ fn main() {
             Command::Exit => break,
             Command::Echo(arguments) => println!("{}", arguments),
             Command::Type(command) => run_type_command(command),
-            Command::Unknown(command) => println!("{}: command not found", command),
+            Command::Unknown(command, arguments) => {
+                let _ = process::Command::new(command)
+                    .args(arguments.split_whitespace())
+                    .status()
+                    .inspect_err(|_| println!("{}: command not found", command));
+            }
         }
     }
 }
@@ -28,7 +34,7 @@ enum Command<'a> {
     Exit,
     Echo(&'a str),
     Type(&'a str),
-    Unknown(&'a str),
+    Unknown(&'a str, &'a str),
 }
 
 impl<'a> From<&'a str> for Command<'a> {
@@ -38,15 +44,15 @@ impl<'a> From<&'a str> for Command<'a> {
         match command {
             "exit" => Self::Exit,
             "echo" => Self::Echo(arguments.trim()),
-            "type" => Self::Type(arguments.trim().into()),
-            unknown => Self::Unknown(unknown.trim().into()),
+            "type" => Self::Type(arguments.trim()),
+            unknown => Self::Unknown(unknown.trim(), arguments.trim()),
         }
     }
 }
 
 fn run_type_command(arguments: &str) {
     match arguments.into() {
-        Command::Unknown(command) => {
+        Command::Unknown(command, _) => {
             let path = env::var_os("PATH").unwrap();
             let file = env::split_paths(&path)
                 .flat_map(fs::read_dir)

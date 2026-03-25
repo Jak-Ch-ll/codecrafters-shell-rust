a3a3588d2131389c7864569db8eae3b9a0ee3929
J.c <Jak-Ch-ll@mailbox.org>
2026-03-25 12:39:15 +0100

    refactor: inline parse function

--- src/arguments.rs
+++ src/arguments.rs
@@ -10,7 +10,21 @@ impl Arguments {
 
 impl From<&str> for Arguments {
     fn from(value: &str) -> Self {
-        Self(parse(value))
+        Self(
+            value
+                .replace("''", "")
+                .split('\'')
+                .enumerate()
+                .flat_map(|(i, part)| {
+                    if i.is_multiple_of(2) {
+                        Box::new(part.split_whitespace()) as Box<dyn Iterator<Item = &str>>
+                    } else {
+                        Box::new(iter::once(part))
+                    }
+                })
+                .map(String::from)
+                .collect(),
+        )
     }
 }
 
@@ -18,22 +32,6 @@ impl std::fmt::Display for Arguments {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0.join(" "))
     }
-}
-
-fn parse(arguments: &str) -> Vec<String> {
-    arguments
-        .replace("''", "")
-        .split('\'')
-        .enumerate()
-        .flat_map(|(i, part)| {
-            if i.is_multiple_of(2) {
-                Box::new(part.split_whitespace()) as Box<dyn Iterator<Item = &str>>
-            } else {
-                Box::new(iter::once(part))
-            }
-        })
-        .map(String::from)
-        .collect()
 }
 
 #[cfg(test)]
@@ -44,62 +42,62 @@ mod tests {
 
     #[test]
     fn it_returns_an_empty_vec_on_emtpy_input() {
-        let result = parse("");
+        let result = Arguments::from("");
 
-        assert!(result.is_empty());
+        assert!(result.as_slice().is_empty());
     }
 
     #[test]
     fn it_returns_a_single_argument_as_is() {
-        let result = parse("foo");
-        assert_eq!(result, vec!["foo"]);
+        let result = Arguments::from("foo");
+        assert_eq!(result.as_slice(), vec!["foo"]);
     }
 
     #[test]
     fn it_trims_whitespace_around_a_single_argument() {
-        let result = parse("   foo    ");
-        assert_eq!(result, vec!["foo"]);
+        let result = Arguments::from("   foo    ");
+        assert_eq!(result.as_slice(), vec!["foo"]);
     }
 
     #[test]
     fn it_returns_multiple_arguments_separated_by_whitespace() {
-        let result = parse("    foo     bar     ");
-        assert_eq!(result, vec!["foo", "bar"]);
+        let result = Arguments::from("    foo     bar     ");
+        assert_eq!(result.as_slice(), vec!["foo", "bar"]);
     }
 
     #[test]
     fn it_returns_a_single_quoted_input_without_the_quotes() {
-        let result = parse("'foo'");
-        assert_eq!(result, vec!["foo"]);
+        let result = Arguments::from("'foo'");
+        assert_eq!(result.as_slice(), vec!["foo"]);
     }
 
     #[test]
     fn it_removes_whitespace_around_quoted_input() {
-        let result = parse("   'foo'    ");
-        assert_eq!(result, vec!["foo"]);
+        let result = Arguments::from("   'foo'    ");
+        assert_eq!(result.as_slice(), vec!["foo"]);
     }
 
     #[test]
     fn it_keeps_whitespace_within_quotes() {
-        let result = parse("'foo   bar'");
-        assert_eq!(result, vec!["foo   bar"])
+        let result = Arguments::from("'foo   bar'");
+        assert_eq!(result.as_slice(), vec!["foo   bar"])
     }
 
     #[test]
     fn it_handles_quotes_after_an_unquoted_element() {
-        let result = parse("foo  bar    'bar   baz'");
-        assert_eq!(result, vec!["foo", "bar", "bar   baz"]);
+        let result = Arguments::from("foo  bar    'bar   baz'");
+        assert_eq!(result.as_slice(), vec!["foo", "bar", "bar   baz"]);
     }
 
     #[test]
     fn it_concatenates_adjacent_quoted_elements() {
-        let result = parse("'foo''bar'");
-        assert_eq!(result, vec!["foobar"]);
+        let result = Arguments::from("'foo''bar'");
+        assert_eq!(result.as_slice(), vec!["foobar"]);
     }
 
     #[test]
     fn it_removes_emtpy_quotes() {
-        let result = parse("foo''bar");
-        assert_eq!(result, vec!["foobar"]);
+        let result = Arguments::from("foo''bar");
+        assert_eq!(result.as_slice(), vec!["foobar"]);
     }
 }
